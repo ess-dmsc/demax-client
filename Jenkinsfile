beeb07b587f7a4d329fd57a5f8b99542d9641dc1
jeremias.hillerberg <jeremias.hillerberg@esss.se>
2018-11-14 11:14:45 +0100

    test

---
+++ Jenkinsfile
@@ -0,0 +1,3 @@
+
+node('docker') {stage('Checkout'){checkout scm}
+                stage('Unit Test User Office '){sh "docker build  .  -t uo" sh 'docker run  -t uo npm test'}}
--- src/app/pages/imprint/imprint.component.spec.ts
+++ src/app/pages/imprint/imprint.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MaterialModule} from '../../material/material.module';
 import { ImprintComponent } from './imprint.component';
 
 describe('ImprintComponent', () => {
@@ -8,7 +8,8 @@ describe('ImprintComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImprintComponent ]
+      declarations: [ ImprintComponent ],
+        imports: [MaterialModule]
     })
     .compileComponents();
   }));
