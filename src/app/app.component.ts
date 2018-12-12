f0a6cf209e302ddd9045e3a02937d86d344eb2e0
jeremias.hillerberg <jeremias.hillerberg@esss.se>
2018-12-12 18:32:55 +0100

    frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
 import { AppConfig } from "./app-config.module";
 import { APP_CONFIG } from "./app-config.module";
-import { FormControl } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { AuthService } from "./services/auth.service";
 
 @Component({
@@ -13,13 +13,20 @@ export class AppComponent implements AfterViewChecked {
 	title = 'demax-client';
 	opened: boolean;
 	mode = new FormControl('over');
+	options: FormGroup;
 
 	constructor(
+		fb: FormBuilder,
 		public auth: AuthService,
 		private changeDetector: ChangeDetectorRef,
 		@Inject(APP_CONFIG) private appConfig: AppConfig
 
 	) {
+	this.options = fb.group({
+		bottom: 0,
+		fixed: false,
+		top: 0
+	});
 	}
 
 	ngAfterViewChecked() {
