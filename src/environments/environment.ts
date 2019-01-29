670964712c41deaf643897e1edaa226db2801b2f
jeremias.hillerberg <jeremias.hillerberg@esss.se>
2019-01-29 11:29:45 +0100

    port change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: false,
-	demaxBaseUrl: "http://127.0.0.1:8080",
-	fileserverBaseURL: "http://127.0.0.1:8889",
+	demaxBaseUrl: "http://127.0.0.1:3000",
+	fileserverBaseURL: "http://127.0.0.1:3000",
 	externalAuthEndpoint: "/auth/msad",
 	archiveWorkflowEnabled: true,
 	editMetadataEnabled: true,
