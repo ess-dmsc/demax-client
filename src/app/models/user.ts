b3f544221892baee214e282abec4ee648c194f9c
Jeremias <40340942+localghost-8080@users.noreply.github.com>
2019-01-30 15:43:22 +0100

    Update user.ts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,14 +1,14 @@
 export class User {
 	_id?: string;
-	firstName: string;
-	lastName:  string;
-	phone:  string;
-	email:  string;
-	password:  string;
+	firstName?: string;
+	lastName?:  string;
+	phone?:  string;
+	email?:  string;
+	password?:  string;
 	role?:  string;
-	employer:  string;
-	industry:  string;
-	jobTitle:  string;
-	hasConsentedToGdpr: boolean;
-	hasConsentedToEmails: boolean;
+	employer?:  string;
+	industry?:  string;
+	jobTitle?:  string;
+	hasConsentedToGdpr?: boolean;
+	hasConsentedToEmails?: boolean;
 }
