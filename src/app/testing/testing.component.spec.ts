2530a3fbf168dd965cb340123b7dbecd41c4e9ac
Gareth Murphy <garethcmurphy@gmail.com>
2019-02-06 11:52:42 +0100

    add providers

--- src/app/testing/testing.component.spec.ts
+++ src/app/testing/testing.component.spec.ts
@@ -1,18 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { TestingComponent } from './testing.component';
-import { MaterialModule } from '../external/material.module';
+import { TestingComponent } from "./testing.component";
+import { MaterialModule } from "../external/material.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
 
-describe('TestingComponent', () => {
+describe("TestingComponent", () => {
   let component: TestingComponent;
   let fixture: ComponentFixture<TestingComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [TestingComponent],
-      imports: [MaterialModule]
-    })
-    .compileComponents();
+      providers: [{ provide: HttpClient }, { provide: Router }],
+      imports: [MaterialModule, FormsModule, ReactiveFormsModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,7 +24,7 @@ describe('TestingComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
