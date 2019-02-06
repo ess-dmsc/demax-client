86815e1b4fefd8f07176db7ad529594818e4ecfb
Gareth Murphy <garethcmurphy@gmail.com>
2019-02-06 11:56:30 +0100

    add animations module import

--- src/app/testing/testing.component.spec.ts
+++ src/app/testing/testing.component.spec.ts
@@ -5,16 +5,18 @@ import { MaterialModule } from "../external/material.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
 import { Router } from "@angular/router";
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { BrowserAnimationsModule, NoopAnimationsModule } from "@angular/platform-browser/animations";
 
 describe("TestingComponent", () => {
   let component: TestingComponent;
   let fixture: ComponentFixture<TestingComponent>;
-
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [TestingComponent],
-      providers: [{ provide: HttpClient }, { provide: Router }],
-      imports: [MaterialModule, FormsModule, ReactiveFormsModule]
+      providers: [{ provide: HttpClient }, { provide: Router }, {provide: JwtHelperService}],
+      imports: [MaterialModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, NoopAnimationsModule]
     }).compileComponents();
   }));
 
