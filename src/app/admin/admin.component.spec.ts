06a28950dc11c08a4d8d85006bba621c151348b7
Gareth Murphy <garethcmurphy@gmail.com>
2019-02-06 11:59:28 +0100

    add http client

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
 import { MaterialModule } from '../external/material.module';
+import { HttpClient } from '@angular/common/http';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
@@ -10,7 +11,8 @@ describe('AdminComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AdminComponent],
-      imports: [MaterialModule]
+      imports: [MaterialModule],
+      providers: [{ provide: HttpClient}]
     })
     .compileComponents();
   }));
