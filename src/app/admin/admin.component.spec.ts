4aa7166b894f67ca3d8794d6515f352dfc539e7a
Jeremias <40340942+localghost-8080@users.noreply.github.com>
2019-02-06 18:11:09 +0100

    Update admin.component.spec.ts

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -27,8 +27,4 @@ describe('AdminComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
