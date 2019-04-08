a361743fe7c639a1cf178dfdf469b3444db6b54c
Jeremias <40340942+localghost-8080@users.noreply.github.com>
2019-04-08 12:43:37 +0200

    Update proposal-admin.component.ts

--- src/app/admin/proposal-admin/proposal-admin.component.ts
+++ src/app/admin/proposal-admin/proposal-admin.component.ts
@@ -4,32 +4,43 @@ import { Proposal } from "../../models/proposal";
 import { APP_CONFIG, AppConfig } from "../../app-config.module";
 
 @Component({
-  selector: 'app-proposal-admin',
-  templateUrl: './proposal-admin.component.html',
-  styleUrls: [ './proposal-admin.component.css']
+	selector: 'app-proposal-admin',
+	templateUrl: './proposal-admin.component.html',
+	styleUrls: [ './proposal-admin.component.css' ]
 })
 export class ProposalAdminComponent implements OnInit {
 
 	proposals: Proposal[] = [];
 	url = this.appConfig.demaxBaseUrl;
-
+	isEditing = false;
+	isEditingMetaData = false;
 	isLoading = true;
-	displayedProposalColumns: string[] = [ 'proposalId', 'experimentTitle', 'categories', 'dateCreated', 'mainProposer', 'submitted','options' ]
+	displayedProposalColumns: string[] = [ 'proposalId', 'experimentTitle', 'categories', 'dateCreated', 'mainProposer', 'submitted', 'options' ]
 
-	constructor(	@Inject(APP_CONFIG) private appConfig: AppConfig,
-	                private proposalService: ProposalService) { }
+	constructor(
+		@Inject(APP_CONFIG) private appConfig: AppConfig,
+		private proposalService: ProposalService
+	) {
+	}
 
-  ngOnInit() {
-	  this.getProposals();
-  }
+	ngOnInit() {
+		this.getProposals();
+	}
 
 	getProposals() {
 		this.proposalService.adminGetProposals().subscribe(
-			data => this.proposals = data,
-			error => console.log(error),
-			() => this.isLoading = false
+			data => {
+				this.proposals = data;
+				this.isLoading = false
+			},
+			error => {
+				console.log(error);
+				this.isLoading = false
+			}
 		)
 	}
+
+
 	deleteProposal(proposal: Proposal) {
 		if(window.confirm('Are you sure you want to delete ' + proposal.mainProposer.email + '?')) {
 			this.proposalService.deleteProposal(proposal).subscribe(
