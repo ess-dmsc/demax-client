b8c2557c745da854a029aa7b53ed6223c08ae778
jeremias.hillerberg <jeremias.hillerberg@esss.se>
2019-05-03 10:53:13 +0200

    removed node-sass

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
     import { BrowserModule } from '@angular/platform-browser';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     
     @NgModule({
  bootstrap: [AppComponent],
@@ -16,6 +17,8 @@ import { HttpClientModule } from '@angular/common/http';
  GraphQLModule,
  
  HttpClientModule,
+ 
+ BrowserAnimationsModule,
  
         ]
     })
